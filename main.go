89561c6aaa4b531694620d8c5706d5300c2dce1f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-21 16:20:27 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"image"
+	"image/gif"
 	"image/jpeg"
+	"image/png"
 	"log"
 	"os"
+	"path/filepath"
+)
+
+var (
+	imageFilePath = os.Args[1]
 )
 
 func main() {
-	path := "image.jpeg"
-	file, err := os.Open(path)
+	imageformatChoice()
+}
+
+func imageformatChoice() {
+	fileExtension := filepath.Ext(imageFilePath)
+	switch fileExtension {
+	case ".jpeg":
+		jpegImage()
+	case ".jpg":
+		jpegImage()
+	case ".png":
+		pngImage()
+	case ".gif":
+		gifImage()
+	default:
+		fmt.Println("Error: Image format not supported")
+	}
+}
+
+func jpegImage() {
+	file, err := os.Open(imageFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,12 +45,54 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	outfile, err := os.Create(path)
+	outfile, err := os.Create(imageFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer outfile.Close()
 	jpeg.Encode(outfile, img, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func pngImage() {
+	file, err := os.Open(imageFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	outfile, err := os.Create(imageFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+	png.Encode(outfile, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func gifImage() {
+	file, err := os.Open(imageFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	outfile, err := os.Create(imageFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+	gif.Encode(outfile, img, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
