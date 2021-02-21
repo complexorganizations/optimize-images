60d436931313bcc4dfd4a13fd067197c10b60bd6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-21 11:38:25 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,23 +8,23 @@ import (
 )
 
 func main() {
-	path := "./image.jpeg"
+	path := "image.jpeg"
 	file, err := os.Open(path)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 	defer file.Close()
 	img, _, err := image.Decode(file)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 	outfile, err := os.Create(path)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 	defer outfile.Close()
 	jpeg.Encode(outfile, img, nil)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 }
